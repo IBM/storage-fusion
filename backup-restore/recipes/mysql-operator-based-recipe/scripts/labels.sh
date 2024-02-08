38d1fa027c63510273a9bb299d7b693302c5a682
ashish-gupta39 <ashish.gupta39@ibm.com>
2024-02-08 11:29:43 +0530

    Updated label for mysql-operator based recipe
    Signed-off-by: ashish-gupta39 <ashish.gupta39@ibm.com>

--- backup-restore/recipes/mysql-operator-based-recipe/scripts/labels.sh
+++ backup-restore/recipes/mysql-operator-based-recipe/scripts/labels.sh
@@ -1,6 +1,19 @@
-oc label customresourcedefinitions crdbclusters.crdb.cockroachlabs.com custom-label=cockroach-operator-crd
-oc label clusterrole cockroach-operator-role app=cockroach-operator
-oc label clusterrolebinding cockroach-operator-rolebinding app=cockroach-operator
-oc label service cockroach-operator-webhook-service app=cockroach-operator
-oc label mutatingwebhookconfiguration cockroach-operator-mutating-webhook-configuration app=cockroach-operator
-oc label validatingwebhookconfiguration cockroach-operator-validating-webhook-configuration app=cockroach-operator
+oc label crd innodbclusters.mysql.oracle.com custom-label=mysql-operator-crd
+oc label crd mysqlbackups.mysql.oracle.com custom-label=mysql-operator-crd
+oc label crd clusterkopfpeerings.zalando.org custom-label=mysql-operator-crd
+oc label crd kopfpeerings.zalando.org custom-label=mysql-operator-crd
+
+oc label clusterrole mysql-operator custom-label=mysql-operator
+oc label clusterrole mysql-sidecar custom-label=mysql-operator
+oc label clusterrolebinding mysql-operator-rolebinding custom-label=mysql-operator
+oc label clusterkopfpeering mysql-operator custom-label=mysql-operator
+
+oc label serviceaccount mysql-operator-sa custom-label=mysql-operator
+
+oc label deployment mysql-operator custom-label=mysql-operator
+
+oc label secret mysql-c1-root-user-creds custom-label=mysql-innodbcluster 
+
+oc label secret mysql-c1-privsecrets custom-label=mysql-innodbcluster-post
+oc label secret mysql-c1-router custom-label=mysql-innodbcluster-post
+oc label secret mysql-c1-backup custom-label=mysql-innodbcluster-post
