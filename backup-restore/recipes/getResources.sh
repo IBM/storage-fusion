3ed2c790285ba0f013188d1123e7e662db7432f8
Sandeep Prajapati <sandeep.prajapati1@ibm.com>
2024-03-20 00:45:16 -0700

    Use job name instead of job id
    Signed-off-by: Sandeep Prajapati <sandeep.prajapati1@ibm.com>

--- backup-restore/recipes/getResources.sh
+++ backup-restore/recipes/getResources.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # This script returns resource details that are backed up or restored by a job in IBM Storage Fusion Backup & Restore service.
-# Usage: getResources.sh (backup | restore) <job id>
-#        e.g. getResources.sh backup 9c1e7f2d-fd74-4bcc-b28e-6cb82785913a
+# Usage: getResources.sh (backup | restore) <job name>
+#        e.g. getResources.sh backup filebrowser-filebrowser-policy-apps.hostname-202402161744
 
 if [[ -n $2 ]]; then
 	if [[ $1 = "restore" ]]; then
@@ -11,6 +11,7 @@ if [[ -n $2 ]]; then
 	fi
 
 	fusionNs=$(oc get spectrumfusion -A --no-headers | cut -d" " -f1)
+        uid=$(oc get f$1 $2 -n $fusionNs -o json | jq '.metadata.uid' | tr -d '"')
     brNs=$(oc -n "$fusionNs" get fusionserviceinstance ibm-backup-restore-service-instance -o json |jq -rc '[(.spec.parameters[]|"\n",.name,.value)]|@csv' | tr -d '"' | grep ",namespace," | cut -d"," -f3)
 
     if [[ -z "$brNs" ]]; then
@@ -23,10 +24,10 @@ if [[ -n $2 ]]; then
 		echo "Error: Backup-service pod is not running";exit
     fi
 
-	oc exec -it $podName -n $brNs -- curl -k https://backup-service:9443/dataprotection/backup-service/v1/logs/$2/resources$queryParam | jq
+	oc exec -it $podName -n $brNs -- curl -k https://backup-service:9443/dataprotection/backup-service/v1/logs/$uid/resources$queryParam | jq
 
 	echo "===== CSV format ====="
-	oc exec -it $podName -n $brNs -- curl -k https://backup-service:9443/dataprotection/backup-service/v1/logs/$2/resources$queryParam | jq -r '[.logs] | @sh' 
+	oc exec -it $podName -n $brNs -- curl -k https://backup-service:9443/dataprotection/backup-service/v1/logs/$uid/resources$queryParam | jq -r '[.logs] | @sh'
 else
-	echo "Usage: " $0 " (backup | restore) <job id>"
+	echo "Usage: " $0 " (backup | restore) <job name>"
 fi
