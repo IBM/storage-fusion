6d61af1ed04f7ee67650349d7dcd5bcc2b8353fe
Sandeep Prajapati <sandeep.prajapati1@ibm.com>
2024-03-29 10:10:45 -0700

    Added necessary changes for MacOS and Linux execution
    Signed-off-by: Sandeep Prajapati <sandeep.prajapati1@ibm.com>

--- backup-restore/recipes/getRecipeWorkflow.sh
+++ backup-restore/recipes/getRecipeWorkflow.sh
@@ -48,7 +48,14 @@ if [[ -n $2 ]]; then
 	fi
 
 	fusionNs=$(oc get spectrumfusion -A --no-headers | cut -d" " -f1)
-        [[ -v job_name ]] && job_uid=$(oc get f$job_type $job_name -n $fusionNs -o json | jq '.metadata.uid' | tr -d '"')
+        uname=$(uname)
+        case "$uname" in
+          "Darwin")
+             [[ "${job_name}" ]] && job_uid=$(oc get f$job_type $job_name -n $fusionNs -o json | jq '.metadata.uid' | tr -d '"') ;;
+          "Linux")
+             [ "${job_name+set}" ] && job_uid=$(oc get f$job_type $job_name -n $fusionNs -o json | jq '.metadata.uid' | tr -d '"') ;;
+          *) echo "Error: Unsupported platform $uname"; exit 1; ;;
+        esac
 	brNs=$(oc -n "$fusionNs" get fusionserviceinstance ibm-backup-restore-service-instance -o json |jq -rc '[(.spec.parameters[]|"\n",.name,.value)]|@csv' | tr -d '"' | grep ",namespace," | cut -d"," -f3)
 
 	if [[ -z "$brNs" ]]; then
