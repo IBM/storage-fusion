92e52249a9d28bbba1c4e0093142ad3e67126a17
liyjcdl <158542505+lisa9019@users.noreply.github.com>
2024-08-22 11:30:02 +0800

    Update applypatch-281.sh

--- backup-restore/hotfixes/2.8.1/applypatch-281.sh
+++ backup-restore/hotfixes/2.8.1/applypatch-281.sh
@@ -6,7 +6,7 @@ exec &> >(tee -a $LOG)
 echo "Logging output in $LOG"
 
 # Check for Backup & Restore namespace
-BR_NS=$(oc get dataprotectionagent -A --no-headers -o custom-columns=NS:metadata.namespace)
+BR_NS=$(oc get dataprotectionserver -A --no-headers -o custom-columns=NS:metadata.namespace 2> /dev/null)
 ISF_NS=$(oc get spectrumfusion -A -o custom-columns=NS:metadata.namespace --no-headers)
 
 if [ -z "$ISF_NS" ]; then
@@ -15,8 +15,14 @@ if [ -z "$ISF_NS" ]; then
 fi
 
 if [ -n "$BR_NS" ]; then
-    echo "This is Spoke."
+    echo "This is Hub. No need to apply the script."
+    exit 0
+    
+else
+    echo "This is spoke. "
     echo "Saving original guardian-configmap yaml."
+
+    BR_NS=$(oc get dataprotectionagent -A --no-headers -o custom-columns=NS:metadata.namespace)
     
     if oc get configmap -n "$BR_NS" guardian-configmap -o yaml > guardian-configmap-original.yaml; then
         echo "Original guardian-configmap saved successfully."
@@ -30,14 +36,9 @@ if [ -n "$BR_NS" ]; then
     if oc set data -n "$BR_NS" cm/guardian-configmap datamoverJobpodMemoryLimit='15000Mi' \
                       datamoverJobpodMemoryRequest='4000Mi' \
                       datamoverJobpodMemoryRequestRes='4000Mi'; then
-        echo "Memory settings updated successfully."
+        echo "Resource settings updated finished."
     else
         echo "ERROR: Failed to update memory settings. Exiting."
         exit 1
     fi
-
-else
-    # Not a spoke cluster, exiting.
-    echo "This isn't spoke. No need to apply the script."
-    exit 0
 fi
