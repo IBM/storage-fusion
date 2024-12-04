aa5796b2d16f33d485ba06391360294b7cc18df4
Ajay Lunawat <100528036+ajaylun@users.noreply.github.com>
2024-12-04 11:23:35 -0800

    Add 2.8.2 patch. (#133)
    * Add 2.8.2 patch.
    Update 2.8.1 post install patch.
    
    Signed-off-by: Ajay Lunawat <100528036+ajaylun@users.noreply.github.com>
    
    * Add MINIO patch for 2.8.0.
    
    Signed-off-by: Ajay Lunawat <100528036+ajaylun@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Ajay Lunawat <100528036+ajaylun@users.noreply.github.com>

--- backup-restore/hotfixes/2.8.1/br-post-install-patch281.sh
+++ backup-restore/hotfixes/2.8.1/br-post-install-patch281.sh
@@ -14,7 +14,31 @@ if [ -z "$ISF_NS" ]; then
     exit 1
 fi
 
-BR_NS=$(oc get dataprotectionagent -A --no-headers -o custom-columns=NS:metadata.namespace)
+BR_NS=$(oc get dataprotectionserver -A --no-headers -o custom-columns=NS:metadata.namespace)
+if [ -n "$BR_NS" ]
+ then
+ HUB=true
+else
+   BR_NS=$(oc get dataprotectionagent -A --no-headers -o custom-columns=NS:metadata.namespace)
+fi
+
+if [ -z "$BR_NS" ] 
+ then
+    echo "ERROR: No B&R installation found. Exiting."
+    exit 1
+fi
+AGENTCSV=$(oc -n "$BR_NS" get csv -o name | grep ibm-dataprotectionagent)
+VERSION=$(oc -n "$BR_NS" get "$AGENTCSV" -o custom-columns=:spec.version --no-headers)
+if [ -z "$VERSION" ] 
+ then
+    echo "ERROR: Could not get B&R version. Exiting"
+    exit 1
+fi
+if [[ $VERSION != 2.8.1* ]]
+ then
+    echo "ERROR: This patch appiles to B&R version 2.8.1 only. You have $VERSION"
+    exit 1
+fi
 
 if (oc -n "$BR_NS" get csv ibm-dataprotectionagent.v2.8.1 -o yaml > ibm-dataprotectionagent.v2.8.1-csv-original.yaml)
    then
@@ -55,44 +79,80 @@ fi
 echo "Patching dbr-controller deployment..."
 echo "Scaling down dbr-controller deployment..."
 oc scale deployment dbr-controller -n "$BR_NS" --replicas=0
-oc patch deployment/dbr-controller -n $BR_NS -p '{"spec":{"template":{"spec":{"containers":[{"name":"dbr-controller","image":"cp.icr.io/cp/fbr/guardian-transaction-manager@sha256:656433641ceda21ee45469500b2b7040983c48d1e3ee50f809be5ab6f3ab527d"}]}}}}'
+oc patch deployment/dbr-controller -n $BR_NS -p '{"spec":{"template":{"spec":{"containers":[{"name":"dbr-controller","image":"cp.icr.io/cp/fbr/guardian-transaction-manager@sha256:be77eecb921b2e905e8ea5e0f6ca9b5b6bec76dc4b6cdde223e54e6c42840e97"}]}}}}'
 oc set resources deployment dbr-controller  -n "$BR_NS" --limits memory=3Gi
 echo "Scaling up dbr-controller deployment..."
 oc scale deployment dbr-controller -n "$BR_NS" --replicas=1
 echo "Patching transaction-manager deployment..."
-oc patch deployment/transaction-manager -n $BR_NS -p '{"spec":{"template":{"spec":{"containers":[{"name":"transaction-manager","image":"cp.icr.io/cp/fbr/guardian-transaction-manager@sha256:656433641ceda21ee45469500b2b7040983c48d1e3ee50f809be5ab6f3ab527d"}]}}}}'
+oc patch deployment/transaction-manager -n $BR_NS -p '{"spec":{"template":{"spec":{"containers":[{"name":"transaction-manager","image":"cp.icr.io/cp/fbr/guardian-transaction-manager@sha256:be77eecb921b2e905e8ea5e0f6ca9b5b6bec76dc4b6cdde223e54e6c42840e97"}]}}}}'
 
-if (oc get deployment -n $BR_NS backuppolicy-deployment -o yaml > backuppolicy-deployment.save.yaml)
+if [[ -z "$SKIP_MINIO" ]];
   then
-    echo "Creating backup-policy service account..."
-    oc create sa backup-policy -n $BR_NS
-
-    echo "Creating backup-policy-role cluster role..."
-    oc create clusterrole backup-policy-role --verb=get --resource=configmaps
-
-    echo "Creating backup-policy-rolebinding cluster role binding..."
-    oc adm policy add-cluster-role-to-user backup-policy-role -z backup-policy  -n $BR_NS --rolebinding-name=backup-policy-rolebinding
-
-    echo "Patching backuppolicy-deployment service account..."
-    oc patch deployment backuppolicy-deployment -n $BR_NS -p '{"spec":{"template":{"spec":{"serviceAccountName":"backup-policy"}}}}'
-
-    echo "Patching backuppolicy-deployment image..."
-    oc patch deployment backuppolicy-deployment -n $BR_NS -p '{"spec":{"template":{"spec":{"containers":[{"name":"backuppolicy-container","image":"cp.icr.io/cp/fbr/guardian-backup-policy@sha256:f7bcf21292258b1e2299a58bccc53886548fd05d5ab714756f4e364195085a5b"}]}}}}'
-else
-    echo "ERROR: Failed to save original backuppolicy-deployment. Skipped updates."
+    echo "Saving old guardian-minio image to old-minio-image.txt"
+    oc get statefulset guardian-minio -n $BR_NS -o jsonpath="{.spec.template.spec.containers[0].image}" >> old-minio-image.txt  
+    echo "Updating statefulset/guardian-minio image to quay.io/minio/minio@sha256:ea15e53e66f96f63e12f45509d2d2d8fad774808debb490f48508b3130bd22d3"
+    oc set image statefulset/guardian-minio -n $BR_NS minio=quay.io/minio/minio@sha256:ea15e53e66f96f63e12f45509d2d2d8fad774808debb490f48508b3130bd22d3
 fi
 
-if (oc get deployment -n $BR_NS backup-location-deployment -o yaml > backup-location-deployment.save.yaml)
+if [ -n "$HUB" ]
   then
-    echo "Patching backup-location-deployment image..."
-    oc patch deployment backup-location-deployment -n $BR_NS -p '{"spec":{"template":{"spec":{"containers":[{"name":"backup-location-container","image":"cp.icr.io/cp/fbr/guardian-backup-location@sha256:b0e2cb55082192780bd6ad02fe19c71d478b213817c64689562869256110af3b"}]}}}}'
-else
-    echo "ERROR: Failed to save original backup-location-deployment. Skipped updates."
+    echo "Apply patches to hub..."
+
+    if (oc get deployment -n $BR_NS backuppolicy-deployment -o yaml > backuppolicy-deployment.save.yaml)
+      then
+        echo "Creating backup-policy service account..."
+        oc create sa backup-policy -n $BR_NS
+
+        echo "Creating backup-policy-role cluster role..."
+        oc create clusterrole backup-policy-role --verb=get --resource=configmaps
+
+        echo "Creating backup-policy-rolebinding cluster role binding..."
+        oc adm policy add-cluster-role-to-user backup-policy-role -z backup-policy  -n $BR_NS --rolebinding-name=backup-policy-rolebinding
+
+        echo "Patching backuppolicy-deployment service account..."
+        oc patch deployment backuppolicy-deployment -n $BR_NS -p '{"spec":{"template":{"spec":{"serviceAccountName":"backup-policy"}}}}'
+
+        echo "Patching backuppolicy-deployment image..."
+        oc patch deployment backuppolicy-deployment -n $BR_NS -p '{"spec":{"template":{"spec":{"containers":[{"name":"backuppolicy-container","image":"cp.icr.io/cp/fbr/guardian-backup-policy@sha256:f7bcf21292258b1e2299a58bccc53886548fd05d5ab714756f4e364195085a5b"}]}}}}'
+    else
+        echo "ERROR: Failed to save original backuppolicy-deployment. Skipped updates."
+    fi
+
+    if (oc get deployment -n $BR_NS backup-location-deployment -o yaml > backup-location-deployment.save.yaml)
+      then
+        echo "Patching backup-location-deployment image..."
+        oc patch deployment backup-location-deployment -n $BR_NS -p '{"spec":{"template":{"spec":{"containers":[{"name":"backup-location-container","image":"cp.icr.io/cp/fbr/guardian-backup-location@sha256:9e9ff3fb6d3c56b1932ee553b374b093adbf037a55a70ae83b359f262029e23e"}]}}}}'
+    else
+        echo "ERROR: Failed to save original backup-location-deployment. Skipped updates."
+    fi
+
+    if (oc get deployment -n $BR_NS backup-service -o yaml > backup-service-deployment.save.yaml)
+      then
+        echo "Patching backup-service image..."
+        oc patch deployment backup-service -n $BR_NS -p '{"spec":{"template":{"spec":{"containers":[{"name":"backup-service","image":"cp.icr.io/cp/fbr/guardian-backup-service@sha256:1bf6b6b093499c14c27fd819637b992999beb0eb5e9cc9dc6feac154b8c7dbda"}]}}}}'
+    else
+        echo "ERROR: Failed to save original backup-service deployment. Skipped updates."
+    fi
+
+    if (oc get recipes.spp-data-protection.isf.ibm.com fusion-control-plane -n $ISF_NS -o yaml > fusion-control-plane-recipe.save.yaml)
+      then
+        echo "Patching fusion-control-plane recipe..."
+        oc patch recipes.spp-data-protection.isf.ibm.com fusion-control-plane -n $ISF_NS --type=merge -p '{"spec":{"hooks":[{"name":"fbr-hooks","nameSelector":"transaction-manager.*","namespace":"${FBR_NAMESPACE}","onError":"fail","ops":[{"command":"[\"/usr/src/app/code/.venv/bin/python3\",\"/usr/src/app/code/ctl-plane.pyc\",\"backup\",\"uid=${BACKUP_ID}\"]","container":"transaction-manager","name":"export-backup-inventory"},{"command":"[\"/usr/src/app/code/.venv/bin/python3\",\"/usr/src/app/code/ctl-plane.pyc\",\"restore\",\"uid=${BACKUP_ID}\"]","container":"transaction-manager","name":"restore-backup-inventory"},{"command":"[\"/usr/src/app/code/.venv/bin/python3\",\"/usr/src/app/code/ctl-plane.pyc\",\"deleteCRs\"]","container":"transaction-manager","name":"deleteCRs"}],"selectResource":"pod","singlePodOnly":true,"timeout":28800,"type":"exec"},{"name":"isf-dp-operator-hook","nameSelector":"transaction-manager.*","namespace":"${FBR_NAMESPACE}","onError":"fail","ops":[{"command":"[\"/usr/src/app/code/.venv/bin/python3\",\"/usr/src/app/code/patch-isd-dp-cm.pyc\",\"${PARENT_NAMESPACE}\",\"isf-data-protection-config\",\"DisableWebhook\"]","container":"transaction-manager","name":"disable-webhook"},{"command":"[\"/usr/src/app/code/.venv/bin/python3\",\"/usr/src/app/code/patch-isd-dp-cm.pyc\",\"${PARENT_NAMESPACE}\",\"isf-data-protection-config\",\"Recover\"]","container":"transaction-manager","name":"quiesce-isf-dp-controller"},{"command":"[\"/usr/src/app/code/.venv/bin/python3\",\"/usr/src/app/code/patch-isd-dp-cm.pyc\",\"${PARENT_NAMESPACE}\",\"isf-data-protection-config\",\"Normal\"]","container":"transaction-manager","name":"unquiesce-isf-dp-controller"}],"selectResource":"pod","singlePodOnly":true,"type":"exec"}]}}'
+    else
+        echo "ERROR: Failed to save original fusion-control-plane recipe. Skipped updates."
+    fi
 fi
+
 echo "Please verify that these pods in $BR_NS namespace have successfully restarted after hotfix update:"
 echo "     guardian-dm-controller-manager"
 echo "     dbr-controller"
 echo "     transaction-manager"
-echo "     backup-location-deployment"
-echo "     backuppolicy-deployment"
+if [ -n "$HUB" ]
+  then
+    echo "     backup-location-deployment"
+    echo "     backuppolicy-deployment"
+    echo "     backup-service deployment"
+fi
+
+
 
