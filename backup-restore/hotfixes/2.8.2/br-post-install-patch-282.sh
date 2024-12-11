9849622eace1d015e6dc4e8cce4ad13b19b2b905
Lana Khorosheva <lanak@us.ibm.com>
2024-12-11 12:15:42 -0800

    Script updates
    Signed-off-by: Lana Khorosheva <lanak@us.ibm.com>

--- backup-restore/hotfixes/2.8.2/br-post-install-patch-282.sh
+++ backup-restore/hotfixes/2.8.2/br-post-install-patch-282.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 # Run this script on hub and spoke clusters to apply the latest hotfixes for 2.8.2 release.
+# Refer to https://supportcontent.ibm.com/support/pages/ibm-storage-fusion-282-hotfix for additional information.
+# Version 12-11-2024
 
-LOG=/tmp/br-post-install-patch-282_$$_log.txt
+mkdir -p /tmp/br-post-install-patch-282
+if [ "$?" -ne 0 ] 
+then DIR=/tmp/br-post-install-patch-282
+else DIR=/tmp
+fi
+LOG=$DIR/br-post-install-patch-282_$$_log.txt
 exec &> >(tee -a $LOG)
 echo "Writing output of br-post-install-patch-282.sh script to $LOG"
 
@@ -12,12 +19,12 @@ if [ -z "$ISF_NS" ]; then
     exit 1
 fi
 
-BR_NS=$(oc get dataprotectionserver -A --no-headers -o custom-columns=NS:metadata.namespace)
+BR_NS=$(oc get dataprotectionserver -A --no-headers -o custom-columns=NS:metadata.namespace 2>/dev/null)
 if [ -n "$BR_NS" ]
  then
  HUB=true
 else
-   BR_NS=$(oc get dataprotectionagent -A --no-headers -o custom-columns=NS:metadata.namespace)
+   BR_NS=$(oc get dataprotectionagent -A --no-headers -o custom-columns=NS:metadata.namespace 2>/dev/null)
 fi
 
 if [ -z "$BR_NS" ] 
@@ -34,7 +41,7 @@ if [ -z "$VERSION" ]
 fi
 if [[ $VERSION != 2.8.2* ]]
  then
-    echo "ERROR: This patch appiles to B&R version 2.8.2 only. You have $VERSION"
+    echo "ERROR: This patch applies to B&R version 2.8.2 only. You have $VERSION"
     exit 1
 fi
 
@@ -42,7 +49,7 @@ if [ -n "$HUB" ]
   then
     echo "Apply patches to hub..."
 
-   if (oc get deployment -n $BR_NS job-manager -o yaml > job-manager-deployment.save.yaml)
+   if (oc get deployment -n $BR_NS job-manager -o yaml > $DIR/job-manager-deployment.save.yaml)
       then
         echo "Patching job-manager-deployment image..."
         oc patch deployment job-manager -n $BR_NS -p '{"spec":{"template":{"spec":{"containers":[{"name":"job-manager","image":"cp.icr.io/cp/fbr/guardian-job-manager@sha256:5a99629999105bdc83862f4bf37842b8004dfb3db9eea20b07ab7e39e95c8edc"}]}}}}'
@@ -50,7 +57,7 @@ if [ -n "$HUB" ]
         echo "ERROR: Failed to save original job-manager-deployment. Skipped updates."
     fi
 
-    if (oc get deployment -n $BR_NS backup-location-deployment -o yaml > backup-location-deployment.save.yaml)
+    if (oc get deployment -n $BR_NS backup-location-deployment -o yaml > $DIR/backup-location-deployment.save.yaml)
       then
         echo "Patching backup-location-deployment image..."
         oc patch deployment backup-location-deployment -n $BR_NS -p '{"spec":{"template":{"spec":{"containers":[{"name":"backup-location-container","image":"cp.icr.io/cp/fbr/guardian-backup-location@sha256:c737450b02a9f415a4c4eea6cc6a67ce0723a8bf5c08ce41469c847c5b598e16"}]}}}}'
@@ -58,7 +65,7 @@ if [ -n "$HUB" ]
         echo "ERROR: Failed to save original backup-location-deployment. Skipped updates."
     fi
 
-    if (oc get deployment -n $BR_NS backuppolicy-deployment -o yaml > backuppolicy-deployment.save.yaml)
+    if (oc get deployment -n $BR_NS backuppolicy-deployment -o yaml > $DIR/backuppolicy-deployment.save.yaml)
       then
         echo "Creating backup-policy service account..."
         oc create sa backup-policy -n $BR_NS
@@ -78,7 +85,7 @@ if [ -n "$HUB" ]
         echo "ERROR: Failed to save original backuppolicy-deployment. Skipped updates."
     fi
 
-    if (oc get deployment -n $BR_NS backup-service -o yaml > backup-service-deployment.save.yaml)
+    if (oc get deployment -n $BR_NS backup-service -o yaml > $DIR/backup-service-deployment.save.yaml)
       then
         echo "Patching backup-service image..."
         oc patch deployment backup-service -n $BR_NS -p '{"spec":{"template":{"spec":{"containers":[{"name":"backup-service","image":"cp.icr.io/cp/fbr/guardian-backup-service@sha256:742367383260fa25fdf1a7cf94a78267361519720869bb6cceac7476b5d5fab3"}]}}}}'
@@ -86,7 +93,7 @@ if [ -n "$HUB" ]
         echo "ERROR: Failed to save original backup-service deployment. Skipped updates."
     fi
 
-    if (oc get recipes.spp-data-protection.isf.ibm.com fusion-control-plane -n $ISF_NS -o yaml > fusion-control-plane-recipe.save.yaml)
+    if (oc get recipes.spp-data-protection.isf.ibm.com fusion-control-plane -n $ISF_NS -o yaml > $DIR/fusion-control-plane-recipe.save.yaml)
       then
         echo "Patching fusion-control-plane recipe..."
         oc patch recipes.spp-data-protection.isf.ibm.com fusion-control-plane -n $ISF_NS --type=merge -p '{"spec":{"hooks":[{"name":"fbr-hooks","nameSelector":"transaction-manager.*","namespace":"${FBR_NAMESPACE}","onError":"fail","ops":[{"command":"[\"/usr/src/app/code/.venv/bin/python3\",\"/usr/src/app/code/ctl-plane.pyc\",\"backup\",\"uid=${BACKUP_ID}\"]","container":"transaction-manager","name":"export-backup-inventory"},{"command":"[\"/usr/src/app/code/.venv/bin/python3\",\"/usr/src/app/code/ctl-plane.pyc\",\"restore\",\"uid=${BACKUP_ID}\"]","container":"transaction-manager","name":"restore-backup-inventory"},{"command":"[\"/usr/src/app/code/.venv/bin/python3\",\"/usr/src/app/code/ctl-plane.pyc\",\"deleteCRs\"]","container":"transaction-manager","name":"deleteCRs"}],"selectResource":"pod","singlePodOnly":true,"timeout":28800,"type":"exec"},{"name":"isf-dp-operator-hook","nameSelector":"transaction-manager.*","namespace":"${FBR_NAMESPACE}","onError":"fail","ops":[{"command":"[\"/usr/src/app/code/.venv/bin/python3\",\"/usr/src/app/code/patch-isd-dp-cm.pyc\",\"${PARENT_NAMESPACE}\",\"isf-data-protection-config\",\"DisableWebhook\"]","container":"transaction-manager","name":"disable-webhook"},{"command":"[\"/usr/src/app/code/.venv/bin/python3\",\"/usr/src/app/code/patch-isd-dp-cm.pyc\",\"${PARENT_NAMESPACE}\",\"isf-data-protection-config\",\"Recover\"]","container":"transaction-manager","name":"quiesce-isf-dp-controller"},{"command":"[\"/usr/src/app/code/.venv/bin/python3\",\"/usr/src/app/code/patch-isd-dp-cm.pyc\",\"${PARENT_NAMESPACE}\",\"isf-data-protection-config\",\"Normal\"]","container":"transaction-manager","name":"unquiesce-isf-dp-controller"}],"selectResource":"pod","singlePodOnly":true,"type":"exec"}]}}'
@@ -95,7 +102,7 @@ if [ -n "$HUB" ]
     fi
 fi
 
-if (oc get deployment -n $BR_NS transaction-manager -o yaml > transaction-manager-deployment.save.yaml)
+if (oc get deployment -n $BR_NS transaction-manager -o yaml > $DIR/transaction-manager-deployment.save.yaml)
   then
     echo "Patching transaction-manager image..."
     oc patch deployment/transaction-manager -n $BR_NS -p '{"spec":{"template":{"spec":{"containers":[{"name":"transaction-manager","image":"cp.icr.io/cp/fbr/guardian-transaction-manager@sha256:0ae46fc2f6e744f79f81005579f4dcb7c9981b201f239433bf1e132f9c89c8cd"}]}}}}'
@@ -109,4 +116,15 @@ if [[ -z "$SKIP_MINIO" ]];
     oc get statefulset guardian-minio -n $BR_NS -o jsonpath="{.spec.template.spec.containers[0].image}" >> old-minio-image.txt
     echo "Updating statefulset/guardian-minio image to quay.io/minio/minio@sha256:ea15e53e66f96f63e12f45509d2d2d8fad774808debb490f48508b3130bd22d3"
     oc set image statefulset/guardian-minio -n $BR_NS minio=quay.io/minio/minio@sha256:ea15e53e66f96f63e12f45509d2d2d8fad774808debb490f48508b3130bd22d3
+fi
+
+echo "Please verify that these pods in $BR_NS namespace have successfully restarted after hotfix update:"
+echo "     guardian-minio"
+echo "     transaction-manager"
+if [ -n "$HUB" ]
+  then
+    echo "     job-manager"
+    echo "     backup-location-deployment"
+    echo "     backuppolicy-deployment"
+    echo "     backup-service"
 fi
