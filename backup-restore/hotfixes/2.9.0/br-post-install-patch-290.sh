1baa5d9d66aa9d607584433cad3331a10e466e35
Eduardo Esteban <eesteban@us.ibm.com>
2025-03-18 12:42:31 -0700

    virtualmachinesnapshot status
    Signed-off-by: Eduardo Esteban <eesteban@us.ibm.com>

--- backup-restore/hotfixes/2.9.0/br-post-install-patch-290.sh
+++ backup-restore/hotfixes/2.9.0/br-post-install-patch-290.sh
@@ -121,7 +121,7 @@ if [[ "$VERSION" == 2.9.0* ]]; then
     if (oc get deployment -n $BR_NS transaction-manager -o yaml > $DIR/transaction-manager-deployment.save.yaml)
     then
         echo "Patching deployment/transaction-manager image..."
-        oc patch deployment/transaction-manager -n $BR_NS -p '{"spec":{"template":{"spec":{"containers":[{"name":"transaction-manager","image":"cp.icr.io/cp/bnr/guardian-transaction-manager@sha256:3e28cda75450285980a2f3ad61fba8c12786c908cd743bba70f056793b050d43"}]}}}}'
+        oc patch deployment/transaction-manager -n $BR_NS -p '{"spec":{"template":{"spec":{"containers":[{"name":"transaction-manager","image":"cp.icr.io/cp/bnr/guardian-transaction-manager@sha256:33ceae1dd8632993952b74eaf01ae72b5911be87f64b675de269161628b87d35"}]}}}}'
     else
         echo "ERROR: Failed to save original transaction-manager deployment. Skipped updates."
     fi
