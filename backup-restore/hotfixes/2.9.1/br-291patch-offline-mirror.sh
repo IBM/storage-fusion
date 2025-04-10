7eac7ca839e760e0a81abf076ff1172f8a856c04
Michael Fruchtman <msfrucht@us.ibm.com>
2025-04-10 12:04:33 -0700

    Fixed and improved offline copy script which was not working in initial commit.
    Copy offline script is now extensible for additional containers and replacing with new images more easily.
    
    Cod review revisions simplifying the conditional statements
    
    Signed-off-by: Michael Fruchtman <msfrucht@us.ibm.com>

--- backup-restore/hotfixes/2.9.1/br-291patch-offline-mirror.sh
+++ backup-restore/hotfixes/2.9.1/br-291patch-offline-mirror.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 LOG=/tmp/$(basename $0)_log.txt
 exec &> >(tee -a $LOG)
-echo "Logging to $LOG"
+echo -e "Logging to $LOG\n"
 
 if [ -z "$1" ]
  then
@@ -12,4 +12,14 @@ TARGET_PATH="$1"
 export TARGET_PATH
 set -e
 
-skopeo copy --insecure-policy --preserve-digests --all docker://cp.icr.io/cp/bnr/guardian-transaction-manager@sha256:b407e1c6585cc38938d52b750dddef57a97846edc4752b37da55014d1b9ef732
+TRANSACTIONMANAGER=guardian-transaction-manager@sha256:b407e1c6585cc38938d52b750dddef57a97846edc4752b37da55014d1b9ef732
+
+declare -a IMAGES=(
+  $TRANSACTIONMANAGER
+)
+
+for IMAGE in "${IMAGES[@]}"; do
+  DESTINATION=docker://$TARGET_PATH/cp/bnr/$IMAGE
+  echo -e "Copying\n Image: $IMAGE\n Destination: docker://$TARGET_PATH/cp/bnr/$IMAGE\n"
+  skopeo copy --insecure-policy --preserve-digests --all docker://cp.icr.io/cp/bnr/$IMAGE $DESTINATION
+done
