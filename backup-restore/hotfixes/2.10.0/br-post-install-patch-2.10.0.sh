91635fc14ad81c85b2f56104f8f4a868514575b3
Eduardo Esteban <eesteban@us.ibm.com>
2025-06-30 17:25:24 -0700

    add dry-run

--- backup-restore/hotfixes/2.10.0/br-post-install-patch-2.10.0.sh
+++ backup-restore/hotfixes/2.10.0/br-post-install-patch-2.10.0.sh
@@ -26,7 +26,10 @@ else
     patch_usage
     exit 1
 fi
+
 EXPECTED_VERSION=2.10.0
+
+DRY_RUN=true # uncomment for dry run
 
 if (mkdir -p /tmp/br-post-install-patch-2.10.0); then
     DIR=/tmp/br-post-install-patch-2.10.0
@@ -47,12 +50,89 @@ check_cmd() {
 }
 
 update_hotfix_configmap() {
-    hotfix="hotfix-"${EXPECTED_VERSION}.${HOTFIX_NUMBER}
+    hotfix=$1
     applied_on=$(date '+%Y-%m-%dT%T')
-    if (oc -n "$BR_NS" get configmap bnr-hotfixes 1>/dev/null 2>&1); then
-        oc -n "$BR_NS" patch configmap bnr-hotfixes --type=json -p "[{\"op\": \"add\", \"path\": \"/data/${hotfix}-applied-on\", \"value\": \"${applied_on}\"}]"
+    if (oc -n "$BR_NS" get configmap bnr-hotfixes -o yaml 1>$DIR/bnr-hotfixes.save.yaml 2>&1); then
+        patch="[{\"op\": \"add\", \"path\": \"/data/${hotfix}-applied-on\", \"value\": \"${applied_on}\"}]"
+        [ -z "$DRY_RUN" ] && oc -n "$BR_NS" patch configmap bnr-hotfixes --type=json -p "${patch}"
+        [ -n "$DRY_RUN" ] && oc -n "$BR_NS" patch configmap bnr-hotfixes --type=json -p "${patch}" --dry-run=client -o yaml >$DIR/bnr-hotfixes.patch.yaml
     else
-        oc -n "$BR_NS" create configmap bnr-hotfixes --from-literal=${hotfix}-applied-on="${applied_on}"
+        [ -z "$DRY_RUN" ] && oc -n "$BR_NS" create configmap bnr-hotfixes --from-literal="${hotfix}"-applied-on="${applied_on}"
+        [ -n "$DRY_RUN" ] && oc -n "$BR_NS" create configmap bnr-hotfixes --from-literal="${hotfix}"-applied-on="${applied_on}" --dry-run=client -o yaml >$DIR/bnr-hotfixes.patch.yaml
+    fi
+
+}
+
+set_deployment_image() {
+    name=$1
+    container=$2
+    image=$3
+    if (oc -n "$BR_NS" get deployment/"${name}" -o yaml >$DIR/"${name}".save.yaml); then
+        echo "Patching deployment/${name} image..."
+        [ -z "$DRY_RUN" ] && oc -n "$BR_NS" set image deployment/"${name}" "${container}"="${image}"
+        [ -n "$DRY_RUN" ] && oc -n "$BR_NS" set image deployment/"${name}" "${container}"="${image}" --dry-run=client -o yaml >$DIR/"${name}".patch.yaml
+        oc -n "$BR_NS" rollout status --timeout=65s deployment/"${name}"
+    else
+        echo "ERROR: Failed to save original deployment/${name}. Skipped updates."
+    fi
+}
+
+update_backuplocation_role() {
+    BSL_ROLES=$(
+        cat <<EOF
+- apiGroups:
+  - config.openshift.io
+  resources:
+  - clusterversions
+  verbs:
+    - get
+    - list
+- apiGroups:
+  - application.isf.ibm.com
+  resources:
+  - clusters
+  verbs:
+    - get
+    - list
+EOF
+    )
+    echo "Patching clusterrole backup-location-role-${BR_NS} ..."
+    oc get clusterrole backup-location-role-"${BR_NS}" -o yaml >$DIR/clusterrole-backup-location-role.save.yaml
+    [ -z "$DRY_RUN" ] && echo -e "$(cat $DIR/clusterrole-backup-location-role.save.yaml)\n${BSL_ROLES}" | oc apply -f -
+    [ -n "$DRY_RUN" ] && echo -e "$(cat $DIR/clusterrole-backup-location-role.save.yaml)\n${BSL_ROLES}" >$DIR/clusterrole-backup-location-role.patch.yaml
+
+}
+
+set_velero_image() {
+    image=$1
+    if (oc -n "$BR_NS" get dpa velero -o yaml >$DIR/velero.save.yaml); then
+        echo "Patching deployment/velero image..."
+        patch="[{\"op\": \"replace\", \"path\": \"/spec/unsupportedOverrides/veleroImageFqin\", \"value\":\"${image}\"}]"
+        [ -z "$DRY_RUN" ] && oc -n "$BR_NS" patch dataprotectionapplication.oadp.openshift.io velero --type='json' -p="${patch}"
+        [ -n "$DRY_RUN" ] && oc -n "$BR_NS" patch dataprotectionapplication.oadp.openshift.io velero --type='json' -p="${patch}" --dry-run=client -o yaml >$DIR/velero.patch.yaml
+        echo "Velero Deployement is restarting with replacement image"
+        oc wait --namespace "$BR_NS" deployment.apps/velero --for=jsonpath='{.status.readyReplicas}'=1
+    fi
+}
+
+update_isf_operator_csv() {
+    name=$1
+    image=$2
+    if (oc get csv -n "$ISF_NS" "$name" -o yaml >$DIR/"$name".save.yaml); then
+        echo "Scaling down isf-data-protection-operator-controller-manager deployment..."
+        [ -z "$DRY_RUN" ] && oc scale deployment -n "$ISF_NS" isf-data-protection-operator-controller-manager --replicas=0
+
+        echo "Patching clusterserviceversion/$name..."
+        index=$(oc get csv -n "$ISF_NS" "$name" -o json | jq '[.spec.install.spec.deployments[].name] | index("isf-data-protection-operator-controller-manager")')
+        patch="[{\"op\":\"replace\", \"path\":\"/spec/install/spec/deployments/${index}/spec/template/spec/containers/0/image\", \"value\":\"${image}\"}]"
+
+        [ -z "$DRY_RUN" ] && oc patch csv -n "$ISF_NS" "$name" --type='json' -p "${patch}"
+        [ -n "$DRY_RUN" ] && oc patch csv -n "$ISF_NS" "$name" --type='json' -p "${patch}" --dry-run=client -o yaml >$DIR/"$name".patch.yaml
+
+        echo "Scaling up isf-data-protection-operator-controller-manager deployment..."
+        [ -z "$DRY_RUN" ] && oc scale deployment -n "$ISF_NS" isf-data-protection-operator-controller-manager --replicas=1
+    else
+        echo "ERROR: Failed to save original clusterserviceversion/$name. Skipped updates."
     fi
 }
 
@@ -102,45 +182,41 @@ fi
 if [ -n "$HUB" ]; then
     echo "Apply patches to hub..."
 
-    if (oc get deployment -n "$BR_NS" backup-location-deployment -o yaml >$DIR/backup-location-deployment.save.yaml); then
-        echo "Patching deployment/backup-location-deployment image..."
-        oc set image deployment/backup-location-deployment -n "$BR_NS" backup-location-container=IMAGE-guardian-backup-location
-        oc rollout status --namespace "$BR_NS" --timeout=65s deployment/backup-location-deployment
-    else
-        echo "ERROR: Failed to save original backup-location-deployment. Skipped updates."
-    fi
+    update_backuplocation_role
 
-    if (oc get deployment -n "$BR_NS" guardian-dp-operator-controller-manager -o yaml >$DIR/guardian-dp-operator-controller-manager.save.yaml); then
-        echo "Patching deployment/guardian-dp-operator-controller-manager image..."
-        oc set image deployment/guardian-dp-operator-controller-manager --namespace "$BR_NS" manager=IMAGE-guardian-dp-operator
-        oc rollout status --namespace "$BR_NS" --timeout=65s deployment/guardian-dp-operator-controller-manager
-    else
-        echo "ERROR: Failed to save original guardian-dp-operator-controller-manager. Skipped updates."
-    fi
+    backuplocation_img=BACKUP-LOCATION-IMAGE
+    set_deployment_image backup-location-deployment backup-location-container "${backuplocation_img}"
+
+    backupservice_img=BACKUP-SERVICE-IMAGE
+    set_deployment_image backup-service backup-service ${backupservice_img}
+
+    backuppolicy_img=BACKUP-POLICY-IMAGE
+    set_deployment_image backuppolicy-deployment backuppolicy-container ${backuppolicy_img}
+
+    guardiandpoperator_img=GUARDIAN-DP-OPERATOR-IMAGE
+    set_deployment_image guardian-dp-operator-controller-manager manager ${guardiandpoperator_img}
+
 fi
 
-if (oc get deployment -n "$BR_NS" transaction-manager -o yaml >$DIR/transaction-manager-deployment.save.yaml); then
-    echo "Patching deployment/transaction-manager image..."
-    oc set image deployment/transaction-manager --namespace "$BR_NS" transaction-manager=IMAGE-guardian-transaction-manager
-    oc rollout status --namespace "$BR_NS" --timeout=65s deployment/transaction-manager
-else
-    echo "ERROR: Failed to save original transaction-manager deployment. Skipped updates."
-fi
+transactionmanager_img=TRANSACTION-MANAGER-IMAGE
+set_deployment_image transaction-manager transaction-manager ${transactionmanager_img}
 
-if (oc --namespace "$BR_NS" get dpa velero -o yaml >$DIR/velero.save.yaml); then
-    echo "Patching deployment/velero image..."
-    oc patch dataprotectionapplication.oadp.openshift.io velero --namespace "$BR_NS" --type='json' -p='[{"op": "replace", "path": "/spec/unsupportedOverrides/veleroImageFqin", "value":"IMAGE-velero"}]'
-    echo "Velero Deployement is restarting with replacement image"
-    oc wait --namespace "$BR_NS" deployment.apps/velero --for=jsonpath='{.status.readyReplicas}'=1
-fi
+velero_img=VELERO-IMAGE
+set_velero_image ${velero_img}
 
-update_hotfix_configmap
+[ "$PATCH" == "HCI" ] && isfdataprotection_img=ISF-DATA-PROTECTION-IMAGE-HCI
+[ "$PATCH" == "SDS" ] && isfdataprotection_img=ISF-DATA-PROTECTION-IMAGE-SDS
+update_isf_operator_csv isf-operator.v2.10.0 "${isfdataprotection_img}"
+
+update_hotfix_configmap "hotfix-"${EXPECTED_VERSION}.${HOTFIX_NUMBER}
 
 echo "Please verify that the pods for the following deployment have successfully restarted:"
 if [ -n "$HUB" ]; then
     printf "  %-${#BR_NS}s: %s\n" "$BR_NS" "backup-location-deployment"
+    printf "  %-${#BR_NS}s: %s\n" "$BR_NS" "backuppolicy-deployment"
+    printf "  %-${#BR_NS}s: %s\n" "$BR_NS" "backup-service"
     printf "  %-${#BR_NS}s: %s\n" "$BR_NS" "guardian-dp-operator-controller-manager"
 fi
-
 printf "  %-${#BR_NS}s: %s\n" "$BR_NS" "transaction-manager"
 printf "  %-${#BR_NS}s: %s\n" "$BR_NS" "velero"
+printf "  %-${#ISF_NS}s: %s\n" "$ISF_NS" "isf-data-protection-operator-controller-manager"
