6f66538c132c42dd02d303a8f4e31fa8dae0f212
Mandeep Jandir <jandirm@us.ibm.com>
2025-07-22 10:13:36 -0700

    IBM Fusion Backup & Restore 2.10.1 Hotfix - 56437

---
+++ backup-restore/hotfixes/2.10.1/br-2101patch-offline-mirror.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+LOG=/tmp/$(basename $0)_log.txt
+exec &> >(tee -a $LOG)
+echo -e "Logging to $LOG\n"
+
+if [ -z "$1" ]
+ then
+   echo "ERROR: Usage: $0 <TARGET_PATH>"
+   exit 1
+fi
+TARGET_PATH="$1"
+export TARGET_PATH
+set -e
+
+
+FBRVELERO=fbr-velero@sha256:910ffee32ec4121df8fc2002278f971cd6b0d923db04d530f31cf5739e08e24c
+
+declare -a IMAGES=(
+  $FBRVELERO
+)
+
+for IMAGE in "${IMAGES[@]}"; do
+  DESTINATION=docker://$TARGET_PATH/cp/bnr/$IMAGE
+  echo -e "Copying\n Image: $IMAGE\n Destination: docker://$TARGET_PATH/cp/bnr/$IMAGE\n"
+  skopeo copy --insecure-policy --preserve-digests --all docker://cp.icr.io/cp/bnr/$IMAGE $DESTINATION
+done
