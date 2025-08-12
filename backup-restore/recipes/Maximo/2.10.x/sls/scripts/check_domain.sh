681f3b4b0859dbbfb80f9773459a92f3eae2fd2d
Jayson Tsingine <tsingine@us.ibm.com>
2025-08-12 16:24:42 -0700

    Removed all maximo experiment and debug scripts.

--- backup-restore/recipes/Maximo/2.10.x/sls/scripts/check_domain.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "=== licenseservices.sls.ibm.com ==="
-oc get licenseservices.sls.ibm.com sls -o json -n ibm-sls | jq ".spec.domain"
--- backup-restore/recipes/Maximo/2.10.x/sls/scripts/update_domain.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-
-echo "=== Update domain licenseservices.sls.ibm.com  ==="
-oc patch licenseservices.sls.ibm.com sls --type merge  -p '{"spec":{"domain":"cpst3.maximo-vpc-test1-98b7318c91b01bd72490e80cc2328915-0000.us-east.containers.appdomain.cloud"}}' -n ibm-sls
