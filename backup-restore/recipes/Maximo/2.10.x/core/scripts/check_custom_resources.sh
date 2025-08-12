9df0213e6b9d0a574add74de841aa7efe52c62ac
Jayson Tsingine <tsingine@us.ibm.com>
2025-08-12 16:24:42 -0700

    Removed all maximo experiment and debug scripts.

--- backup-restore/recipes/Maximo/2.10.x/core/scripts/check_custom_resources.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for i in `oc get crd -l app.kubernetes.io/name=ibm-mas --no-headers | awk '{print $1}' | xargs`; do echo -e "\n=== $i ==="; oc get $i -A;  done
-
-echo -e "\n=== truststores ==="
-oc get truststores -A | grep -v ibm-sls
--- backup-restore/recipes/Maximo/2.10.x/manage/scripts/check_custom_resources.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-for i in `oc get crd -l app.kubernetes.io/name=ibm-mas-manage --no-headers | awk '{print $1}' | xargs`; do echo -e "\n=== $i ==="; oc get $i -A;  done
