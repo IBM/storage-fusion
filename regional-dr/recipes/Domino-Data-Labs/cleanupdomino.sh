bb49d5e353fc8465eb211ea431a9e2738d637b7d
Kenneth P. Salerno <kennethsalerno@yahoo.com>
2026-02-09 10:34:08 -0500

    Adding regional-dr directory and some RDR recipe examples
    Signed-off-by: Kenneth P. Salerno <kennethsalerno@yahoo.com>

---
+++ regional-dr/recipes/Domino-Data-Labs/cleanupdomino.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+for i in domino-compute domino-platform; do
+	echo "removing Deploy and STS from $i..."
+	oc delete -n $i deploy,sts --all
+	echo "removing VolumeReplications from $i..."
+	oc delete vr -n $i --all
+	echo "removing ReplicationSources from $i..."
+	oc delete replicationsources -n $i --all
+done
+for i in domino-compute domino-platform; do
+	echo "removing PVCs from $i..."
+	oc delete pvc -n $i --all
+done
+cat <<EOF
+
+Optional: cleanup endpointslices on target cluster:
+oc delete endpointslices -n domino-platform --all
+oc delete pod -n submariner-operator -lcomponent=submariner-lighthouse
+EOF
