e61da840dea6a10cbb2162ca265b36e18941b068
Kenneth P. Salerno <kennethsalerno@yahoo.com>
2026-02-10 08:06:54 -0500

    Catch condition and retry when VM has snapshot inflight
    Signed-off-by: Kenneth P. Salerno <kennethsalerno@yahoo.com>

--- regional-dr/recipes/Virtual-Machines/cleanupvms.sh
+++ regional-dr/recipes/Virtual-Machines/cleanupvms.sh
@@ -8,12 +8,18 @@ for i in virtualmachines; do
 	oc delete vr -n $i --all
 	echo "Stopping VMs in $i..."
 	for vm in $(oc get vms -n $i -o name | cut -d/ -f2); do
-		virtctl stop -n $i $vm
+		virtctl stop -n $i $vm || {
+			while :; do
+				echo "Pausing for retry in 10 seconds..."
+				sleep 10
+				virtctl stop -n $i $vm && break
+			done
+		}
 	done
 	while :; do
 		test $(oc get vmi -n $i --no-headers | wc -l) -eq 0 && break
-		echo "Pausing for VMs to stop..."
-		sleep 5
+		echo "Pausing 10 seconds for VMs to stop..."
+		sleep 10
 	done
 	echo "removing VMs from $i..."
 	oc delete -n $i vms --all
